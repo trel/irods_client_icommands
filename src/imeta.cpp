9303a5c3487a0f014676fea29e73453ed929629e
Kory Draughn <korydraughn@renci.org>
2020-05-13 14:25:43 -0400

    [irods/irods#4426] imeta qu no longer ignores -z option.

--- src/imeta.cpp
+++ src/imeta.cpp
@@ -1188,11 +1188,10 @@ getInput( char *cmdToken[], int maxTokens ) {
 }
 
 std::tuple<po::parsed_options, po::variables_map>
-parse_program_options (
-    int                                     _argc,
-    const char**                            _argv,
-    rodsArguments_t&                        _rods_args ) {
-
+parse_program_options(int _argc,
+                      const char** _argv,
+                      rodsArguments_t& _rods_args)
+{
     po::options_description global("Global options");
     global.add_options()
         ("help,h",                                             "Show imeta help")
@@ -1231,6 +1230,7 @@ parse_program_options (
         if ( vm.count( "zone_name" ) ) {
             _rods_args.zone = 1;
             _rods_args.zoneName = (char*) vm["zone_name"].as<std::string>().c_str();
+            rstrcpy(zoneArgument, _rods_args.zoneName, sizeof(zoneArgument));
         }
 
         if ( vm.count( "help" ) ) {
@@ -1239,7 +1239,8 @@ parse_program_options (
         }
 
         return std::make_tuple(parsed, vm);
-    } catch ( const po::error& _e ) {
+    }
+    catch (const po::error& _e) {
         std::cout << std::endl
                   << "Error: "
                   << _e.what()
